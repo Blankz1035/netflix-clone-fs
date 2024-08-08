28a94ece9272e8b1fb2544ca76d9338f4d4bc0bf
Blankz1035 <andrew.blankley@hotmail.com>
2024-08-08 20:12:17 +0100

    add user authentication check on app load

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 
 import { BrowserRouter as Router, Routes,  Route } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase/firebase';
 
 
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth){
+        // logged in
+      }
+      else {
+        // logged out.
+      }
+    });
+
+    return unsubscribe;
+  },[])
+
 
   return (
     <div className="app">
