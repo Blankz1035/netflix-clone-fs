d2e74a5ef7f2393623eb8a4ab5e6613387f9bc6a
Blankz1035 <andrew.blankley@hotmail.com>
2024-08-08 20:06:59 +0100

    add signin handler and tested.

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -26,7 +26,14 @@ const SignUpScreen = () => {
 
   const signIn = (e) => {
     e.preventDefault();
-
+    auth.signInWithEmailAndPassword(
+      emailRef.current.value,
+      passwordRef.current.value,
+    )
+    .then((authUser) => {
+      console.log(authUser);
+    })
+    .catch((error) => alert(error.message));
   }
 
   return (
